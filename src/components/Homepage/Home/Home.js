9ddf1366d683bc760b8be41dafb24b32f8745071
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-11-10 19:26:58 +0600

    changing color of navbar item

--- src/components/Homepage/Home/Home.js
+++ src/components/Homepage/Home/Home.js
@@ -9,24 +9,24 @@ const Home = () => {
         count: 1,
     });
     const works = [];
-        resumeData?.work.map(item => {
-            return works.push(item)
-        })
-        useEffect(()=>{
-            works[0] && setSiteInfo({...siteInfo, work:works[0]})
-         },[])
-        if( works.length === siteInfo.count){
-            setSiteInfo({...siteInfo, count:0})
-        }
+    resumeData?.work.map(item => {
+        return works.push(item)
+    })
+    useEffect(()=>{
+        works[0] && setSiteInfo({...siteInfo, work:works[0]})
+        },[])
+    if( works.length === siteInfo.count){
+        setSiteInfo({...siteInfo, count:0})
+    }
     const handleClick = () => {
         setSiteInfo({count:siteInfo.count+1, work:works[siteInfo.count]})
     }
     return (
         <div id="home" style={{background:siteInfo.work?.bgColor, height: "100vh"}} 
-        onClick={()=> handleClick() }
+        onClick={()=> handleClick() } 
         >
-            <Navbar/>
-            <Header />
+            <Navbar color={siteInfo.work?.color} />
+            <Header  />
             <h2> {siteInfo.work?.company} </h2>
             
         </div>
--- src/components/Homepage/Navbar/Navbar.js
+++ src/components/Homepage/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faPhone, faEnvelope, faBars } from '@fortawesome/free-solid-svg-icons';
 
-const Navbar = () => {
+const Navbar = ({color}) => {
     const [active, setActive] = useState(false)
     return (
         <div id="navbar">
@@ -17,44 +17,44 @@ const Navbar = () => {
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <a className="nav-link " href="https://wa.me/+16463732266">
-                                <span> US </span>  <FontAwesomeIcon icon={faWhatsapp} />
+                                <span style={{color:color}}> US </span>  <FontAwesomeIcon style={{color:color}} icon={faWhatsapp} />
                             </a>
                         </li>
                         <li className="nav-item ">
                             <a className="nav-link " href="tel:0502209852">
-                            <span> IL </span> <FontAwesomeIcon icon={faPhone} /> 
+                            <span style={{color:color}}> IL </span> <FontAwesomeIcon style={{color:color}} icon={faPhone} /> 
                             </a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link " href="tel:+16463732266"> 
-                                <span> US </span> <FontAwesomeIcon icon={faPhone} /> 
+                                <span style={{color:color}}> US </span> <FontAwesomeIcon style={{color:color}} icon={faPhone} /> 
                             </a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="https://www.linkedin.com/in/anatoly-raklyar-06034069/">  
-                                <FontAwesomeIcon icon={faLinkedinIn} /> 
+                                <FontAwesomeIcon style={{color:color}} icon={faLinkedinIn} /> 
                             </a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="mailto:traklyar@yahoo.com">  
-                                <FontAwesomeIcon icon={faEnvelope} /> 
+                                <FontAwesomeIcon style={{color:color}} icon={faEnvelope} /> 
                             </a>
                         </li>
                         <div className="navbar-wraper" onClick={() => setActive(!active)}>
                             { !active ?<li className="nav-item">
                                 <div className="nav-link"  href="">  
-                                    <FontAwesomeIcon icon={faBars} /> 
+                                    <FontAwesomeIcon style={{color:color}} icon={faBars} /> 
                                 </div>
                             </li>
                             :<div>
                             <li className="nav-item">
                                 <div className="nav-link"  href="">  
-                                <strong> X </strong>
+                                <strong style={{color:color}}> X </strong>
                                 </div>
                             </li>
                                 <li className="nav-item">
                                     <a className="nav-link" onClick="revert()" href="">  
-                                        <FontAwesomeIcon icon={faGithub} /> 
+                                        <FontAwesomeIcon style={{color:color}} icon={faGithub} /> 
                                     </a>
                                 </li>
                             </div>}
