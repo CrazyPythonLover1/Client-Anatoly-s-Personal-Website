e300cd286b9019a4d5b1e26ea5a3b66df7c51483
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-11-12 21:07:45 +0600

    work initial color

--- src/components/Homepage/Work/Work.js
+++ src/components/Homepage/Work/Work.js
@@ -23,42 +23,40 @@ const Work = ({ work, handleClick }) => {
         audio.pause();
         setActive(!active);
     }
-    const [backgroundColor, setBackgroundColor] = useState(work?.workColor);
+    const [backgroundColor, setBackgroundColor] = useState("#232323");
     useEffect(() => {
-        if (work?.workColor) {
-            setBackgroundColor(work.workColor)
-        } else {
+        if (work?.bgColor) {
             setBackgroundColor(work?.bgColor)
         }
     }, [])
 
     return (
-        <div style={{position:"relative"}}>
-             <div className="col-sm-1 spacer"  onClick={ ()=> handleClick() } >  </div>
-             <div className="col-sm-1 spacer spacer-bottom" onClick={ ()=> handleClick() } >  </div>
-        <div id="work" style={{ backgroundColor: backgroundColor }}>
-           
-            <div className="button-group">
-                <a href="../../../data/Resume/IL.docx" download> <button className="resume" > <span> RESUME   </span>  </button> </a>
-                <a href="../../../data/Resume/US.docx" download> <button className="letter"> <span>   LETTER   </span>  </button> </a>
-                <span className="or"> or </span>
-            </div>
-            <div className="info">
-                <div className="btn-play-pause">
-                    {!active ? <FontAwesomeIcon onClick={() => handlePlayMusic()} icon={faPlay} />
-                        : <FontAwesomeIcon onClick={() => handlePauseMusic()} icon={faPause} />}
-                </div>
-                <span className="company"> <span> $ </span> {work?.company} </span>
-                <span className="position"> <span> $ </span> {work?.position} </span>
-                {work?.startDate && <span className="date"> <span> $ </span> {work?.startDate} </span>}
-                {work?.endDate && <span className="date"> <span> $ </span> {work?.endDate} </span>}
-                {work?.summary && <span className="summary"> <span> $ </span> {work?.summary} </span>}
-                {work?.highlights && <span className="mt-3">   Highlights </span>}
-                {work?.highlights && work?.highlights.map(item => <span> {`->`} {item} </span>)}
+        <div style={{ position: "relative" }}>
+            <div className="col-sm-1 spacer" onClick={() => handleClick()} >  </div>
+            <div className="col-sm-1 spacer spacer-bottom" onClick={() => handleClick()} >  </div>
+            <div id="work" style={{ backgroundColor: backgroundColor }}>
 
+                <div className="button-group">
+                    <a href="../../../data/Resume/IL.docx" download> <button className="resume" > <span> RESUME   </span>  </button> </a>
+                    <a href="../../../data/Resume/US.docx" download> <button className="letter"> <span>   LETTER   </span>  </button> </a>
+                    <span className="or"> or </span>
+                </div>
+                <div className="info">
+                    <div className="btn-play-pause">
+                        {!active ? <FontAwesomeIcon onClick={() => handlePlayMusic()} icon={faPlay} />
+                            : <FontAwesomeIcon onClick={() => handlePauseMusic()} icon={faPause} />}
+                    </div>
+                    <span className="company"> <span> $ </span> {work?.company} </span>
+                    <span className="position"> <span> $ </span> {work?.position} </span>
+                    {work?.startDate && <span className="date"> <span> $ </span> {work?.startDate} </span>}
+                    {work?.endDate && <span className="date"> <span> $ </span> {work?.endDate} </span>}
+                    {work?.summary && <span className="summary"> <span> $ </span> {work?.summary} </span>}
+                    {work?.highlights && <span className="mt-3">   Highlights </span>}
+                    {work?.highlights && work?.highlights.map(item => <span> {`->`} {item} </span>)}
+
+                </div>
             </div>
-        </div>
-        
+
         </div>
     );
 };
